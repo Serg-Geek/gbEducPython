c14445886e34a993b4d9c3f8e0c2c45b8c747573
user <svgordienko@yandex.ru>
2022-10-21 20:51:38 +0400

    add test

--- SeminarPython/SeminarPython001/test.py
+++ SeminarPython/SeminarPython001/test.py
@@ -28,19 +28,31 @@ import time
 
 # print(datetime.today().minute, datetime.today().second,  datetime.today().microsecond, datetime.today())
 
-word = "bottles" 
-for beer_num in range(99, 0, -1): 
-    print(beer_num, word, "of beer on the wall.") 
-    print(beer_num, word, "of beer.") 
-    print("Take one down.") 
-    print("Pass it around.") 
-    time.sleep(4)
-    if beer_num == 1: 
-        print("No more bottles of beer on the wall.") 
-    else: 
-        new_num = beer_num - 1 
-        if new_num == 1: 
-                word = "bottle" 
-        print(new_num, word, "of beer on the wall.") 
-    print()
+# word = "bottles" 
+# for beer_num in range(99, 0, -1): 
+#     print(beer_num, word, "of beer on the wall.") 
+#     print(beer_num, word, "of beer.") 
+#     print("Take one down.") 
+#     print("Pass it around.") 
+#     time.sleep(4)
+#     if beer_num == 1: 
+#         print("No more bottles of beer on the wall.") 
+#     else: 
+#         new_num = beer_num - 1 
+#         if new_num == 1: 
+#                 word = "bottle" 
+#         print(new_num, word, "of beer on the wall.") 
+#     print()
 
+def count_bits(n):
+    while n>0:
+        s=[]
+        s= (str(n %2 ) + s).split
+        n = n // 2
+    count = 0
+    for i in range(s):
+        if s[i] == 1:
+            count  = count+1
+    return count
+
+print(count_bits(1234))
