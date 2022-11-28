f76344de7bc659650b6e18ad2b4e5016bf106734
user <svgordienko@yandex.ru>
2022-11-28 19:19:00 +0400

    del garbage

--- Phonebook/model/test.py
+++
@@ -1,9 +0,0 @@
-
-
-nam = {}
-with open("/home/user/Python/gbEducPython/Phonebook/model/name.txt", "r") as data:
-    # a_nam = data.readline()# get str
-    for line in data:
-        key, *value = line.split('.')
-        nam[key] = value
-print(nam)
